bbf6ba2368fbe81d06ccd2afc576368ef36397bd
Steven Smethurst <funvill@funvill.com>
2013-09-30 16:20:57 -0700

    Update LEDMatrix.cpp

--- LEDMatrix.cpp
+++ LEDMatrix.cpp
@@ -1,12 +1,14 @@
 #include "stdafx.h"
 
-//============================================================================
-// Name        : LEDMatrix.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
+/*****************************************************************************
+ * Name        : LEDMatrix.cpp
+ * Author      : Steven Smethurst (https://github.com/funvill/) 
+ * Version     : 0.01
+ * Copyright   : Creative Commons Attribution-ShareAlike 3.0 Unported License
+ *               http://creativecommons.org/licenses/by-sa/3.0/deed.en_GB
+ * Description : Skeleton code for VHS LED Matrix Badge  
+ *               https://github.com/vhs/vhs-pcbs/tree/master/led-matrix
+*****************************************************************************/
 
 // Key parameters for ATmega168P
 // Flash (Kbytes):   16 Kbytes
@@ -25,21 +27,52 @@
 #define LED_MATRIX_LEDS	 	 2
 
 // 
-#define LED_MATRIX_SIZE 	LED_MATRIX_WIDTH * LED_MATRIX_HIGHT 
+#define LED_MATRIX_LENGTH 	LED_MATRIX_WIDTH * LED_MATRIX_HIGHT 
+#define LED_MATRIX_SIZE		(LED_MATRIX_LENGTH * LED_MATRIX_LEDS) / 8
 
 // ToDo: Change this to a char to add PWM... 
 #define LED_COLOR_VAR		bool 
 #define LED_COLOR_OFF		false
 #define LED_COLOR_ON		true	
 
+class CButtons
+{
+public:
+	enum ButtonTypes { 
+		BUTTON_UP = 0, 
+		BUTTON_RIGHT, 
+		BUTTON_LEFT, 
+		BUTTON_DOWN, 
+		BUTTON_A, 
+		BUTTON_B, 
+	} ; 
+
+	unsigned char m_data ; 
+
+	bool GetButtonState( ButtonTypes button ) {
+		return ( (this->m_data >> button) & 1 ) ; 
+	}
+
+	void Debug() {
+		printf("Buttons down: ");
+		if( this->GetButtonState( BUTTON_UP ) )		{ printf( "UP "); } 
+		if( this->GetButtonState( BUTTON_RIGHT ) )	{ printf( "RIGHT "); } 
+		if( this->GetButtonState( BUTTON_LEFT ) )	{ printf( "LEFT "); } 
+		if( this->GetButtonState( BUTTON_DOWN ) )	{ printf( "DOWN "); } 
+		if( this->GetButtonState( BUTTON_A ) )		{ printf( "A "); } 
+		if( this->GetButtonState( BUTTON_B ) )		{ printf( "B "); } 
+		printf("\n");
+	}
+};
+
 class CLEDMatrix
 {
 	public:
-		unsigned char m_data[ (LED_MATRIX_SIZE * LED_MATRIX_LEDS) / 8 ];
+		unsigned char m_data[ LED_MATRIX_SIZE ];
 
 		CLEDMatrix() { 
 			// Clear the data. 
-			memset( m_data, 0, (LED_MATRIX_SIZE * LED_MATRIX_LEDS) / 8 );
+			memset( m_data, 0, LED_MATRIX_SIZE );
 		}
 
 
@@ -52,7 +85,8 @@ class CLEDMatrix
 		LED_COLOR_VAR GetLEDGreen( unsigned char x, unsigned char y ); 		
 
 		// Length = in bits 
-		bool LoadString( char * data, unsigned short length );
+		bool LoadLEDS( const char * data, const unsigned char length );
+		unsigned char SaveLEDS( char * data, const unsigned char max_length ); 
 
 		// Prints debug information about the LED matrix to the screen. 
 		void Debug(); 
@@ -85,7 +119,7 @@ bool CLEDMatrix::SetLEDGreen( unsigned char x, unsigned char y, LED_COLOR_VAR gr
 		return false;
 	}
 
-	unsigned char offsetByte	= ((x + y*LED_MATRIX_WIDTH) / 8 ) + (LED_MATRIX_SIZE/8) ;
+	unsigned char offsetByte	= ((x + y*LED_MATRIX_WIDTH) / 8 ) + (LED_MATRIX_LENGTH/8) ;
 	unsigned char offsetBit		= (x + y*LED_MATRIX_WIDTH) % 8 ;
 	if( green == LED_COLOR_ON ) {
 		this->m_data[ offsetByte ] |= 1 << offsetBit ; 
@@ -106,29 +140,33 @@ LED_COLOR_VAR CLEDMatrix::GetLEDRed( unsigned char x, unsigned char y ) {
 	return ( ( this->m_data[ offsetByte ] >> offsetBit) & 1 ) ; 
 }
 LED_COLOR_VAR CLEDMatrix::GetLEDGreen( unsigned char x, unsigned char y ) {
-	unsigned char offsetByte	= ((x + y*LED_MATRIX_WIDTH) / 8) + (LED_MATRIX_SIZE/8) ;
+	unsigned char offsetByte	= ((x + y*LED_MATRIX_WIDTH) / 8) + (LED_MATRIX_LENGTH/8) ;
 	unsigned char offsetBit		= (x + y*LED_MATRIX_WIDTH) % 8 ;
 	return ( ( this->m_data[ offsetByte ] >> offsetBit) & 1 ) ; 
 }
 
 void CLEDMatrix::SetAllLEDS( LED_COLOR_VAR red, LED_COLOR_VAR green ) {
 	// Much faster with memset then a double for loop 
-	// ToDo: there is something wrong here 
-	// memset( this->m_data, red, LED_MATRIX_SIZE / 8 );
-	// memset( this->m_data + (LED_MATRIX_SIZE / 8), green, LED_MATRIX_SIZE / 8 );
-
-	
-	// Turn off all the LEDS
-	for( unsigned char offsetWidth = 0 ; offsetWidth < LED_MATRIX_WIDTH; offsetWidth++ ) {
-		for( unsigned char offsetHeight = 0 ; offsetHeight < LED_MATRIX_HIGHT; offsetHeight++ ) {
-			this->SetLED(offsetWidth, offsetHeight, red, green );
-		}	
+	if( red ) { 
+		// On 
+		memset( this->m_data, 0xFF, sizeof( char ) * (LED_MATRIX_LENGTH / 8) );
+	} else {
+		// Off 
+		memset( this->m_data, 0x00, sizeof( char ) * (LED_MATRIX_LENGTH / 8) );
 	}
-	
+
+	// Green 
+	if( green ) { 
+		// On 
+		memset( this->m_data + sizeof( char ) * (LED_MATRIX_LENGTH / 8), 0xFF, sizeof( char ) * (LED_MATRIX_LENGTH / 8) );
+	} else {
+		// Off 
+		memset( this->m_data + sizeof( char ) * (LED_MATRIX_LENGTH / 8), 0x00, sizeof( char ) * (LED_MATRIX_LENGTH / 8) );
+	}
 }
 
 
-int char2int(char input)
+int char2int( const char input)
 {
 	if(input >= '0' && input <= '9') {
 		return input - '0';
@@ -146,7 +184,7 @@ int char2int(char input)
 
 // This function will set up LED glyph from a string made with the online editor.
 // 8x16x2 = 256 LEDs, or 32 BYTES. 64 letters in hex for the entire display. 
-bool CLEDMatrix::LoadString( char * data, unsigned short length ) {
+bool CLEDMatrix::LoadLEDS( const char * data, const unsigned char length ) {
 	// Check the size of the string to make sure we don't get any buffer overflows.
 	// ToDo: 	
 
@@ -167,11 +205,19 @@ bool CLEDMatrix::LoadString( char * data, unsigned short length ) {
 			// Reset the var 
 			output = 0 ; 
 		}
-		// printf( "\noffset=[%d], char=[%d] output=[%d]", offset, data[offset], output);
-		
+	}
+	return true ; 
+}
+
+unsigned char CLEDMatrix::SaveLEDS( char * output, const unsigned char max_length ) {
+	if( max_length < LED_MATRIX_SIZE * 2 + 1 ) {
+		return 0; // Error not enugh space. 
 	}
 
-	return true ; 
+	for( unsigned short offset = 0 ; offset < LED_MATRIX_SIZE ; offset++ ) {
+		sprintf( output+(offset*2), "%02X", this->m_data[ offset ] ); 
+	}
+	return LED_MATRIX_SIZE * 2 ; 
 }
 
 void CLEDMatrix::Debug() {
@@ -205,20 +251,26 @@ void CLEDMatrix::Debug() {
 		}	
 		
 	}
-	printf( "\n\n"); 
+	printf( "\n"); 
+
+	// LED_MATRIX_SIZE converted in to 2 byte hex code with 1 byte for null terminal. 
+	char buffer[LED_MATRIX_SIZE*2+1] ; 
+	this->SaveLEDS( buffer,  LED_MATRIX_SIZE*2+1 );
+	
+	printf( "Code: %s\n\n", buffer ); 
 
 }
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	CLEDMatrix matrix ;
+	
 
 	// Test patterns 
-
 	printf("Red\n");
 	matrix.SetAllLEDS( LED_COLOR_ON, LED_COLOR_OFF ); 
 	matrix.Debug(); 
-
+	
 	printf("Green\n");
 	matrix.SetAllLEDS( LED_COLOR_OFF, LED_COLOR_ON ); 
 	matrix.Debug(); 
@@ -251,9 +303,9 @@ int _tmain(int argc, _TCHAR* argv[])
 		for( unsigned char y = 0 ; y < LED_MATRIX_HIGHT; y++ ) {
 			if( x % 2 == 0 ) { 
 				if( y % 2 == 0 ) {
-					matrix.SetLEDGreen( x, y, LED_COLOR_ON );
-				} else if(x < LED_MATRIX_WIDTH -1 ) {
 					matrix.SetLEDGreen( x+1, y, LED_COLOR_ON );
+				} else if(x < LED_MATRIX_WIDTH -1 ) {
+					matrix.SetLEDGreen( x, y, LED_COLOR_ON );
 				}
 			}
 		}	
@@ -282,7 +334,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	matrix.Debug(); 
 
 	// From string 
-	if( ! matrix.LoadString( "A0B1C2", 6 ) ) {
+	if( ! matrix.LoadLEDS( "A0B1C2", 6 ) ) {
 		printf( "Matrix loaded\n" );
 	}
 	matrix.Debug(); 
